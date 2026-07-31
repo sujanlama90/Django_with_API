f95516fe271bee8235067f63ca7a8747c9dea343
Sujan Lama <sujanlama2323@gmail.com>
2026-07-31 19:47:04 +0545

    fetch weather api

--- fetchApi/weather/views.py
+++ fetchApi/weather/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib import messages
+from 
 import requests
+
 # Create your views here.
 def home(request):
+    API_KEY = config
     if 'city' in request.POST:
         city = request.POST.get('city','kathmandu')
     else:
@@ -9,12 +13,24 @@ def home(request):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=546aa424c7307f3e3c5ebae20a7db814'
     param = {'units':'metric'}
     data = requests.get(url,param).json()
-    temp = data["main"]["temp"]
-    wind = data["wind"]["speed"]
-    weather = data["weather"][0]["description"]
-    weather_icon = data["weather"][0]["icon"]
-    humidity = data["main"]["humidity"]
-    feels_like = data["main"]["feels_like"]
-    pressure = data["main"]["pressure"]
-    country = data["sys"]["country"]
-    return render(request,'index.html',{'temp':temp,'city':city,'wind':wind,"description":weather,'humidity':humidity,'weather_icon':weather_icon,'feels_like':feels_like,'pressure':pressure,"country":country})
+    try:
+        temp = data["main"]["temp"]
+        wind = data["wind"]["speed"]
+        weather = data["weather"][0]["description"]
+        weather_icon = data["weather"][0]["icon"]
+        humidity = data["main"]["humidity"]
+        feels_like = data["main"]["feels_like"]
+        pressure = data["main"]["pressure"]
+        country = data["sys"]["country"]
+        return render(request,'index.html',{'temp':temp,'city':city,'wind':wind,"description":weather,'humidity':humidity,'weather_icon':weather_icon,'feels_like':feels_like,'pressure':pressure,"country":country})
+    except:
+        messages.error(request,"no such  city ")
+        temp = 0
+        wind = 0
+        weather = 0
+        weather_icon = ''
+        humidity = 0
+        feels_like = 0
+        pressure = 0
+        country = "NP"
+        return render(request,'index.html',{'temp':temp,'city':city,'wind':wind,"description":weather,'humidity':humidity,'weather_icon':weather_icon,'feels_like':feels_like,'pressure':pressure,"country":country})
